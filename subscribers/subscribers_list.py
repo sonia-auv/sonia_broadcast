f7b3f890d121b73007dd49e775ff1b6deb73d834
alexemdesgagne <alexandre1998@live.ca>
2020-12-13 22:43:56 -0500

    Added stuff in the readme and new subscriber.

--- subscribers/subscribers_list.py
+++ subscribers/subscribers_list.py
@@ -5,6 +5,6 @@ from subscribers.subscriber import Subscriber
 def get_subscribers_list(ros_connector):
     sub_list = []
     sub_list.append(Subscriber(ros_connector, '/mission_manager/mission_name_msg', 'mission_manager/MissionNameMsg'))
-    sub_list.append(Subscriber(ros_connector, '/proc_control_matlab/waypoint', 'proc_control_matlab/Waypoint'))
+    sub_list.append(Subscriber(ros_connector, '/proc_control_matlab/pose', 'proc_control_matlab/Pose'))
     return sub_list
         
